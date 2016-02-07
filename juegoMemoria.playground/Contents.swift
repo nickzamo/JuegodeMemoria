f8ca6b9d1362e6cd7d27332cb9a3146e2b40e7c9
Nicolas Zamora Ramos <ing.sis.zamora@hotmail.com>
2016-02-07 13:12:45 -0600

    Test

---
+++ juegoMemoria.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+for a in rango
+{
+    
+    if (a%5)==0
+    {
+        print("#\(a)Bingo!!!")
+    }
+    if (a%2)==0
+    {
+        print("#\(a)par")
+    }
+    if ((a%2)>0)
+    {
+        print("#\(a)impar")
+    }
+    
+    
+    switch a
+    {
+    case 30...40:
+        print("#\(a) Viva Swift")
+    default:
+        break
+    }
+    
+    
+}
